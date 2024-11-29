871d4842680a4855c4438fd7dade316090334143
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-11-29 17:26:13 +0500

    userpool uncomment fix

---
+++ frontend/src/aws-exports.ts
@@ -0,0 +1,10 @@
+const awsmobile = {
+  Auth: {
+    Cognito: {
+      region: "us-east-1",
+      userPoolId: "us-east-1_07k0zgIk4",
+      userPoolClientId: "310ph82na07d8has7rii6mscmu",
+    },
+  },
+};
+export default awsmobile;
