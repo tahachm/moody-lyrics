47f9f38f5d547787df879072f4d7bbf95f7288a7
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-12-03 03:12:34 +0500

    lambda update

--- frontend/src/aws-exports.ts
+++ frontend/src/aws-exports.ts
@@ -2,8 +2,8 @@ const awsmobile = {
   Auth: {
     Cognito: {
       region: "us-east-1",
-      userPoolId: "us-east-1_07k0zgIk4",
-      userPoolClientId: "310ph82na07d8has7rii6mscmu",
+      userPoolId: "us-east-1_6EjMvVk6M",
+      userPoolClientId: "5vacjpuoegii8tg1qmuns6rs3u",
     },
   },
 };
